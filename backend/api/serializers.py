6318c3a6bd56cc656e07f51d14517d3f26694e11
Вячеслав Поликарский <polikarskij@yandex.ru>
2023-03-09 00:37:55 +0300

    commit 7 08.03.23

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,8 @@
 import django.contrib.auth.password_validation as validators
-from django.contrib.auth import authenticate
-from drf_base64.fields import Base64ImageField
-from django.shortcuts import get_object_or_404
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.hashers import make_password
+from django.shortcuts import get_object_or_404
+from drf_base64.fields import Base64ImageField
 from rest_framework import serializers
 
 from recipes.models import Ingredient, Tag, Recipe, RecipeIngredient
