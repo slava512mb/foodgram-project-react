eff7e1358bf990f946d9dd6c1eeeca42d0d7d719
Вячеслав Поликарский <polikarskij@yandex.ru>
2023-03-09 00:58:51 +0300

    commit 7 08.03.23

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,13 +3,6 @@ import io
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfgen import canvas
-from django.contrib.auth import get_user_model
-from django.contrib.auth.hashers import make_password
-from django.db.models.aggregates import Count, Sum
-from django.db.models.expressions import Exists, OuterRef, Value
-from django.http import FileResponse
-from django.shortcuts import get_object_or_404
-from djoser.views import UserViewSet
 from rest_framework import generics, status, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -18,6 +11,14 @@ from rest_framework.permissions import (SAFE_METHODS, AllowAny,
                                         IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from django.db.models.aggregates import Count, Sum
+from django.db.models.expressions import Exists, OuterRef, Value
+from django.http import FileResponse
+from django.shortcuts import get_object_or_404
+from djoser.views import UserViewSet
+
 
 from api.filters import IngredientFilter, RecipeFilter
 from api.permissions import IsAdminOrReadOnly
