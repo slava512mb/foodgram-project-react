519d5e8d90f90cfefa8fcc00b0eca34a8002b24e
Вячеслав Поликарский <polikarskij@yandex.ru>
2023-03-09 01:00:01 +0300

    commit 7 08.03.23

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,8 @@
 import io
 
-from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfgen import canvas
+from reportlab.pdfbase.ttfonts import TTFont
 from rest_framework import generics, status, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
