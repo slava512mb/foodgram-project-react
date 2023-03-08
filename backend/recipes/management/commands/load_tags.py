fd9046e2b161fd0a8a4fa35fc53d0030a50ca321
Вячеслав Поликарский <polikarskij@yandex.ru>
2023-03-08 23:46:30 +0300

    commit 7 08.03.23

--- backend/recipes/management/commands/load_tags.py
+++ backend/recipes/management/commands/load_tags.py
@@ -8,8 +8,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         data = [
-            {'name': 'Завтрак', 'color': '#E2A5A4', 'slug': 'breakfast'},
-            {'name': 'Обед', 'color': '#E8DE2F', 'slug': 'lunch'},
-            {'name': 'Ужин', 'color': '#2366E7', 'slug': 'dinner'}]
+            {'name': 'Завтрак',
+             'color': '#E26C2D',
+             'slug': 'breakfast'
+            },
+            {'name': 'Обед',
+             'color': '#49B64E',
+             'slug': 'dinner'
+            },
+            {'name': 'Ужин',
+             'color': '#8775D2',
+             'slug': 'supper'
+            }
+        ]
         Tag.objects.bulk_create(Tag(**tag) for tag in data)
         self.stdout.write(self.style.SUCCESS('Все тэги загружены!'))
