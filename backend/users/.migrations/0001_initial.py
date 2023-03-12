ff9f7da6ad2df09d79228aac69cb69ddaa677864
Вячеслав Поликарский <polikarskij@yandex.ru>
2023-03-13 00:00:36 +0300

    commit 11 12.03.23

--- backend/users/.migrations/0001_initial.py
+++ backend/users/.migrations/0001_initial.py
@@ -78,9 +78,7 @@ class Migration(migrations.Migration):
                     related_name='user_set',
                     related_query_name='user',
                     to='auth.Group',
-                    help_text=('The groups this user belongs to.')
-                              (' A user will get all permissions')
-                              (' granted to each of their groups.'),
+                    help_text='The groups this user belongs to.',
                     verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(
                     blank=True,
