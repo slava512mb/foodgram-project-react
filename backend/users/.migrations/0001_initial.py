6bc0ff538f50b071e81f8dbd37f95d3f561b02d4
Вячеслав Поликарский <polikarskij@yandex.ru>
2023-03-12 22:11:25 +0300

    commit 11 12.03.23

--- backend/users/.migrations/0001_initial.py
+++ backend/users/.migrations/0001_initial.py
@@ -41,7 +41,8 @@ class Migration(migrations.Migration):
                                     ('A user with that')
                                     ('username already exists.')},
                     help_text=('Required. 150 characters')
-                              ('or fewer. Letters, digits and @/./+/-/_ only.'),
+                              ('or fewer. Letters, ')
+                              ('digits and @/./+/-/_ only.'),
                     max_length=150,
                     unique=True,
                     validators=[django.contrib.auth.
@@ -75,7 +76,7 @@ class Migration(migrations.Migration):
                 ('groups', models.ManyToManyField(
                     blank=True,
                     help_text=('The groups this user belongs to. ')
-                              ('A user will get all permissions')
+                              ('A user will get all permissions' )
                               ('granted to each of their groups.'),
                     related_name='user_set',
                     related_query_name='user',
