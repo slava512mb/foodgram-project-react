f8bf76e44f8118e3bb358f520bfad09ccf3dbe9d
Вячеслав Поликарский <polikarskij@yandex.ru>
2023-03-13 23:00:21 +0300

    commit 12 13.03.23

--- backend/api/views.py
+++ backend/api/views.py
@@ -22,8 +22,9 @@ from rest_framework.response import Response
 from api.filters import IngredientFilter, RecipeFilter
 from api.permissions import IsAdminOrReadOnly
 from users.models import (Subscribe)
-from recipes.models import (FavoriteRecipe, Ingredient, Recipe, ShoppingCart,
-                            Tag)
+from recipes.models import (FavoriteRecipe, Ingredient, Recipe,
+                            ShoppingCart, Tag)
+
 from .serializers import (IngredientSerializer, RecipeReadSerializer,
                           RecipeWriteSerializer, SubscribeRecipeSerializer,
                           SubscribeSerializer, TagSerializer, TokenSerializer,
