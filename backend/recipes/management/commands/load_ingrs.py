a32dca8b604493af012751047011ac112c4b37a3
Вячеслав Поликарский <polikarskij@yandex.ru>
2023-03-22 23:46:10 +0300

    commit 29 22.03.23

--- backend/recipes/management/commands/load_ingrs.py
+++ backend/recipes/management/commands/load_ingrs.py
@@ -1,20 +1,33 @@
 import csv
+import os
 
 from django.conf import settings
-from django.core.management import BaseCommand
-
+from django.core.management.base import BaseCommand, CommandError
 from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
     help = 'Загрузка файла ingredients.csv'
 
-    def handle(self, *args, **kwargs):
-        data_path = settings.BASE_DIR
-        with open(
-             f'{data_path}/data/ingredients.csv', 'r',
-             encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            Ingredient.objects.bulk_create(
-                [Ingredient(**data) for data in reader], batch_size=None)
-        self.stdout.write(self.style.SUCCESS('Ингридиенты загружены'))
+    def add_arguments(self, parser):
+        parser.add_argument('filename', default='ingredients.csv', nargs='?',
+                            type=str)
+
+    def handle(self, *args, **options):
+        try:
+            with open(os.path.join(
+                    settings.BASE_DIR,
+                    'data',
+                    options['filename']),
+                    'r',
+                    encoding='utf-8') as f:
+                data = csv.reader(f)
+                for row in data:
+                    name, measurement_unit = row
+                    Ingredient.objects.get_or_create(
+                        name=name,
+                        measurement_unit=measurement_unit
+                    )
+        except FileNotFoundError:
+            raise CommandError('Добавьте файл ingredients.csv в папку data')
+        self.stdout.write(self.style.SUCCESS('Все ингридиенты загружены!'))
