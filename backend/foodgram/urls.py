8fe25fffcb5048e89884e1dda6dd2ada23d5650d
Вячеслав Поликарский <polikarskij@yandex.ru>
2023-03-25 11:09:23 +0300

    commit 38 25.03.23

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('api/', include('api.urls', namespace='api'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT)
